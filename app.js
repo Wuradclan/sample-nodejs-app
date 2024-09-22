16988a14cffe9fa768c437acc3b9c22fe7d7d2e2
WuRAD <morad.aitabdellah@gmail.com>
2024-09-21 18:48:55 -0600

    code to show html page added

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
 var express = require('express');
 var app = express();
+//this line also added
+app.use(express.static('public')); 
 app.get('/', function (req, res) {
   res.send('marker.html');
 });
+// new code to show html page
+app.get("/showfile", (req, res, next) => { 
+  // show the page
+     const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const _retfile = path.join(__dirname, 'public/marker.html');
+ 
+  res.sendFile(_retfile);
+ });
+ // old code
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
